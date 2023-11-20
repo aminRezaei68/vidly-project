4f69a2734a18df1bca085e3828352e8a8a267f95
aminRezaei68 <rezaei.amin1268@gmail.com>
2023-11-20 18:00:20 +0330

    delete the arry and modified

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 
-const Joi = require('joi');
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 const genres = require('./routes/genres');
+
+mongoose.connect('mongodb://127.0.0.1/vidly')
+    .then(() => {console.log('Connecting to the mongoDB.')})
+    .catch(err => {console.error('Can not Connecting to the mongoDB...', err)}); 
 
 app.use(express.json());
 app.use('/api/genres', genres);
