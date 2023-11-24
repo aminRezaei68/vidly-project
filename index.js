bed968cc7ccd0ae3946d46a1899fb093f9e8f05f
aminRezaei68 <rezaei.amin1268@gmail.com>
2023-11-24 13:26:47 +0330

    add movie API to index module

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
+const movies = require('./routes/movies');
 
 mongoose.connect('mongodb://127.0.0.1/vidly')
     .then(() => {console.log('Connecting to the mongoDB.')})
@@ -12,6 +13,7 @@ mongoose.connect('mongodb://127.0.0.1/vidly')
 app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
+app.use('/api/movies', movies);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port} ...`));
