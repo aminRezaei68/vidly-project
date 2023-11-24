27964e228a5f5bcc73ab0828b3013a20f9773bae
aminRezaei68 <rezaei.amin1268@gmail.com>
2023-11-24 13:36:34 +0330

    create route for movies

---
+++ routes/movies.js
@@ -0,0 +1,78 @@
+const {Movie, validate} = require('../models/movie');
+const {Genre} = require('./genres')
+const mongoose = require('mongoose');
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {// get all customers
+    const movies = await Movie.find().sort('title');
+    res.send(movies);
+});
+
+router.get('/:id', async (req, res) => {
+    const movie = await Movies.findById(req.params.id);
+
+    if (!movie) {
+        return res.status(404).send('The ID that you enter is not exist.');
+    }
+
+    res.send(movie);
+});
+
+router.post('/', async (req, res) => {
+    const { error } = validate(req.body);
+    console.log(error);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+        // return res.status(400).send(error.details);
+    }
+    const gener = await Genre.findById(req.body.genreId);
+    if (!genre) {
+        return res.status(400).send('Invalid genre...');
+    }
+
+    let movie = new Movie({
+        title: req.body.title,
+        genre: {
+            _id: genre._id,
+            name: genre.name
+        },
+        numberInStock: req.body.numberInStock,
+        dailyRentalRate: req.body.dailyRentalRate
+    });
+    movie = await movie.save();
+
+    res.send(movie);
+});
+
+router.put('/:id', async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+
+    const movie = await Movie.findByIdAndUpdate(req.params.id, {
+        title: req.body.title,
+        genre: req.body.genre,
+    }, { new: true
+    })
+
+    if (!movie) {
+        return res.status(404).send('The ID that you enter is not exist.');
+    }
+
+    res.send(movie);
+});
+
+router.delete('/:id', async (req, res) => {
+    const movie = await Movie.findByIdAndRemove(req.params.id);
+
+    if (!movie) {
+        return res.status(404).send('The ID that you enter is not exist.');
+    }
+
+    res.send(movie);
+});
+
+module.exports = router;
