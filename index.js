1258d526c85448db84caeea297a44d567043f08c
aminRezaei68 <rezaei.amin1268@gmail.com>
2023-11-24 22:31:48 +0330

    add rental API

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
+const rentals = require('./routes/rentals');
 
 mongoose.connect('mongodb://127.0.0.1/vidly')
     .then(() => {console.log('Connecting to the mongoDB.')})
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 app.use('/api/movies', movies);
+app.use('/api/rentals', rentals);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port} ...`));
