e85c4064c23c94c2b968e729408f09dd51836731
aminRezaei68 <rezaei.amin1268@gmail.com>
2023-11-28 23:49:39 +0330

    add second argument to mongoose.connect because we use old -v of mongoose so the second argument is ({useMongoClient:true})

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
 
-mongoose.connect('mongodb://127.0.0.1/vidly')
+mongoose.connect('mongodb://127.0.0.1/vidly',{useMongoClient:true})
     .then(() => {console.log('Connecting to the mongoDB.')})
     .catch(err => {console.error('Can not Connecting to the mongoDB...', err)}); 
 
