916dfb9c98a551c2b10a39b541a0c1b2712fef1e
aminRezaei68 <rezaei.amin1268@gmail.com>
2023-11-29 01:19:46 +0330

    add Joi and joi-objectId to validate the value of ID's

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 
 const mongoose = require('mongoose');
+const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
 const express = require('express');
 const app = express();
 const genres = require('./routes/genres');
