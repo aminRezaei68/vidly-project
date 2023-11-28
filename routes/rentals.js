2582f1537ac9a11518957eb669c1c4efdf1a6988
aminRezaei68 <rezaei.amin1268@gmail.com>
2023-11-28 22:59:12 +0330

    add Fawn to rentals

--- routes/rentals.js
+++ routes/rentals.js
@@ -2,9 +2,12 @@ const {Rental, validate} = require('../models/rental');
 const {Movie} = require('../models/movie');
 const {Customer} = require('../models/customer');
 const mongoose = require('mongoose');
+const Fawn = require('fawn');
 const express = require('express'); 
-const { required } = require('joi/lib/types/lazy');
+// const { required } = require('joi/lib/types/lazy');
 const router = express.Router();
+
+Fawn.init(mongoose);
 
 router.get('/', async (req, res) => {
     const rentals = await Rental.find().sort('-dateOut');
@@ -35,11 +38,20 @@ router.post('/', async (req, res) => {
             dailyRentalRate: movie.dailyRentalRate
         }
     });
-    rental = await rental.save();
-    movie.numberInStock--;
-    movie.save();
 
-    res.send(rental);
+    try {
+        new Fawn.Task()
+            .save('rentals', rental)
+            .update('movies', {_id: movie._id}, {
+                $inc: {numberInStock: -1}
+            })
+            .run();
+    
+        res.send(rental);
+    }
+    catch (exception) {
+        res.status(500).send('something failed.')
+    }
 });
 
 router.get('/:id', async (req, res) => {
