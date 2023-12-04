71f5ca54786f9dfbe1997ce4141a416946396b95
aminRezaei68 <rezaei.amin1268@gmail.com>
2023-12-04 20:34:13 +0330

    create auth.js route

---
+++ routes/auth.js
@@ -0,0 +1,37 @@
+const Joi = require('joi');
+const bycrypt = require('bcrypt');
+const _ = require('lodash');
+const {User} = require('../models/user');
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+    const {error} = validate(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message)
+    }
+
+    let user = await User.findOne({email : req.body.email});
+    if (!user) {
+        return res.status(400).send('invalid email or passwords.');
+    } 
+
+    const validPassword = await bycrypt.compare(req.body.password, user.password);
+    if (!validPassword) {
+        return res.status(400).send('invalid email or passwords.');
+    }
+
+    res.send(true);
+});
+
+function validate(req) {
+    const schema = {
+        email: Joi.string().min(5).max(255).required().email(),
+        password: Joi.string().min(8).max(255).required()
+    };
+
+    return Joi.validate(req, schema);
+}
+
+module.exports = router; 
