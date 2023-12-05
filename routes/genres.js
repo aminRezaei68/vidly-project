0a8acb2e6c76e2341d225548bb5f7cf62382c553
aminRezaei68 <rezaei.amin1268@gmail.com>
2023-12-05 21:20:08 +0330

    add auth and admin to delete router

--- routes/genres.js
+++ routes/genres.js
@@ -1,4 +1,5 @@
 const auth = require('../middleware/auth');
+const admin = require('../middleware/admin');
 const {Genre, validate} = require('../models/genre');
 const mongoose = require('mongoose');
 const express = require('express');
@@ -51,7 +52,7 @@ router.put('/:id', async (req, res) => {
     res.send(genre);
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', [auth, admin], async (req, res) => {
     const genre = await Genre.findByIdAndRemove(req.params.id);
 
     if (!genre) {
