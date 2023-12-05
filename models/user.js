3ede15268dca7b478647c9b2c0d157fc032bcf17
aminRezaei68 <rezaei.amin1268@gmail.com>
2023-12-05 21:18:10 +0330

    add << isAdmin >> properties to userSchema in user module

--- models/user.js
+++ models/user.js
@@ -21,11 +21,12 @@ const userSchema = new mongoose.Schema({
         type: String,
         minlength: 8,
         maxlength: 1024
-    }
+    },
+    isAdmin: Boolean
 });
 
 userSchema.methods.generateAuthToken = function() {
-    const token = jwt.sign({_id: this._id}, config.get('jwtPrivateKey'));
+    const token = jwt.sign({_id: this._id, isAdmin: this.isAdmin}, config.get('jwtPrivateKey'));
     return token;
 };
 
