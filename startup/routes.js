1cacfe0e3530fcfe44d21887b173f5fe48e812f2
aminRezaei68 <rezaei.amin1268@gmail.com>
2023-12-11 20:41:51 +0330

    modify app argument in routes.js

--- startup/routes.js
+++ startup/routes.js
@@ -7,7 +7,7 @@ const auth = require('../routes/auth');
 const users = require('../routes/users');
 const error = require('../middleware/error');
 
-module.exports = function() {
+module.exports = function(app) {
     app.use(express.json());
     app.use('/api/genres', genres);
     app.use('/api/customers', customers);
