ce853dfb78b41672c39fa73a0195fcc7a82209d9
aminRezaei68 <rezaei.amin1268@gmail.com>
2023-12-11 21:04:53 +0330

    extract logging and errorHandling into separate module

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-require('express-async-errors');
-const winston = require('winston');
-require('winston-mongodb');
 const config = require('config');
 const mongoose = require('mongoose');
 const Joi = require('joi');
@@ -8,28 +5,10 @@ Joi.objectId = require('joi-objectid')(Joi);
 const express = require('express');
 const app = express();
 
+require('./startup/logging')();
 require('./startup/routes')(app);
 require('./startup/db')();
 
-// process.on('uncaughtException', (exception) => { //instade of this we can use: handleExceptions()
-//     winston.error(exception.message, exception);
-//     process.exit(1);
-// });
-
-winston.handleExceptions(
-    new winston.transports.File({ filename: 'uncaughtExceptions.log'}));
-
-process.on('unhandledRejection', (exception) => {
-    // winston.error(exception.message, exception);
-    // process.exit(1); // instade of these to line we can use : throw exexception.
-    throw exception;
-});
-
-winston.add(winston.transports.File, {filename: 'logfile.log'});
-winston.add(winston.transports.MongoDB, {
-    db: 'mongodb://localhost/vidly',
-    level: 'error'
-});
 
 if (!config.get('jwtPrivateKey')) {
     console.error('FATA ERROR: jwtPrivateKey is not define.');
