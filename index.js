8f7a13bf9cd34f5d1054feba8d6a9685efd2731d
aminRezaei68 <rezaei.amin1268@gmail.com>
2023-12-11 21:40:02 +0330

    extract the config section to separate module

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const config = require('config');
 const mongoose = require('mongoose');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
@@ -6,14 +5,9 @@ const express = require('express');
 const app = express();
 
 require('./startup/logging')();
+require('./startup/config')();
 require('./startup/routes')(app);
 require('./startup/db')();
-
-
-if (!config.get('jwtPrivateKey')) {
-    console.error('FATA ERROR: jwtPrivateKey is not define.');
-    process.exit(1)
-}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port} ...`));
