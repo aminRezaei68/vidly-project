d3af3aa5ded5d382c58147f84b1b1c76877003f2
aminRezaei68 <rezaei.amin1268@gmail.com>
2023-12-11 19:09:25 +0330

    extract routes into sepreate module

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 require('express-async-errors');
 const winston = require('winston');
 require('winston-mongodb');
-const error = require('./middleware/error');
 const config = require('config');
 const mongoose = require('mongoose');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const express = require('express');
 const app = express();
-const genres = require('./routes/genres');
-const customers = require('./routes/customers');
-const movies = require('./routes/movies');
-const rentals = require('./routes/rentals');
-const auth = require('./routes/auth');
-const users = require('./routes/users');
+
+require('./startup/routes')(app);
 
 // process.on('uncaughtException', (exception) => { //instade of this we can use: handleExceptions()
 //     winston.error(exception.message, exception);
@@ -44,14 +39,6 @@ mongoose.connect('mongodb://127.0.0.1/vidly',{useMongoClient:true})
     .then(() => {console.log('Connecting to the mongoDB.')})
     .catch(err => {console.error('Can not Connecting to the mongoDB...', err)}); 
 
-app.use(express.json());
-app.use('/api/genres', genres);
-app.use('/api/customers', customers);
-app.use('/api/movies', movies);
-app.use('/api/rentals', rentals);
-app.use('/api/users', users);
-app.use('/api/auth', auth);
-app.use(error);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port} ...`));
