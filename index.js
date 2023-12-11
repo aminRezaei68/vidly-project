167c3f5d23674d18c137788a85b3438835627e42
aminRezaei68 <rezaei.amin1268@gmail.com>
2023-12-11 17:03:01 +0330

    modify uncaughtException and unhandledRejection

--- index.js
+++ index.js
@@ -15,14 +15,18 @@ const rentals = require('./routes/rentals');
 const auth = require('./routes/auth');
 const users = require('./routes/users');
 
-process.on('uncaughtException', (exception) => {
-    console.log('WE GOT AN UNCAUGHT EXCEPTION');
-    winston.error(exception.message, exception);
-});
+// process.on('uncaughtException', (exception) => { //instade of this we can use: handleExceptions()
+//     winston.error(exception.message, exception);
+//     process.exit(1);
+// });
+
+winston.handleExceptions(
+    new winston.transports.File({ filename: 'uncaughtExceptions.log'}));
 
 process.on('unhandledRejection', (exception) => {
-    console.log('WE GOT AN UNHANDLED REJECTION');
-    winston.error(exception.message, exception);
+    // winston.error(exception.message, exception);
+    // process.exit(1); // instade of these to line we can use : throw exexception.
+    throw exception;
 });
 
 winston.add(winston.transports.File, {filename: 'logfile.log'});
